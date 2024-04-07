4334579057471608fef20f1d7996037439ca672e
swastik <swastikasija072@gmail.com>
2024-04-07 21:08:37 +0530

    updated version

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Waviardh: We ride waves with friendship & courage.
 </h1>
-      <p class="mb-8 leading-relaxed">Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid swag typewriter affogato, hella selvage wolf narwhal dreamcatcher.</p>
+      <p class="mb-8 leading-relaxed">In the brotherhood of The Waviardh, we stand united as pillars of strength, bound by loyalty, and forged by shared experiences, where every member is a cornerstone in our journey of growth and solidarity.</p>
       <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Explore Us
       
     </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
@@ -6,10 +7,8 @@ const Navbar = () => {
     <header class="text-gray-400 bg-gray-900 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span class="ml-3 text-xl">Tailblocks</span>
+        <img src={Logo} alt="" class="w-10 h-10 rounded-full" />
+      <span class="ml-3 text-xl">The Waviardh</span>
     </a>
     <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
       <a class="mr-5 hover:text-white">First Link</a>
