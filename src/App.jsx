cc024002418653aae34b0798d1cc688182131093
swastik <swastikasija072@gmail.com>
2024-04-07 19:27:19 +0530

    Home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import './App.css'
+import Home from './components/Home'
 import Navbar from './components/Navbar'
 // import Hero from './components/Hero'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
       {/* <Hero/> */}
     </>
   )
